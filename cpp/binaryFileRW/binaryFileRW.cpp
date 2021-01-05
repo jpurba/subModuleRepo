1403bf5f74ac65d5a81d7903cd706e2b9df66903
Josua.Purba <Josua.Purba@elembio.com>
2021-01-05 12:15:03 -0800

    add folder for example only

---
+++ cpp/binaryFileRW/binaryFileRW.cpp
@@ -0,0 +1,24 @@
+/*
+ * binaryFileRW.cpp
+ *
+ *  Created on: Jan 9, 2019
+ *      Author: jpurba
+ */
+
+
+#include <iostream>
+#include <fstream>
+
+
+//#pragma pack(push, 1) // allign to single bit to avoid padding
+
+
+
+//#pragma pack(pop) // end pragma
+using namespace std;
+int main () {
+
+	cout << "Hello World \n" << endl;
+
+	return 0;
+}
